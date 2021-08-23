6aff74b4d0dd830ca5560417329c654f82b20c4a
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-08-23 07:30:54 +0530

    (feature) Add Count from store to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box, Heading, Text, VStack} from "@chakra-ui/react";
+import {useSelector} from "react-redux";
 
 function App() {
+    const {count} = useSelector(state => state.counter)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <VStack p={4}>
+       <Heading fontSize="6xl">Counter App</Heading>
+        <Box>
+            <Text fontSize="3xl">Count : {count}</Text>
+        </Box>
+   </VStack>
   );
 }
 
