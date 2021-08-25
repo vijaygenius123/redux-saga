819ef1a80b38af8e518496727b4b34f2a52c4ae9
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-08-25 21:13:49 +0530

    (feature) Basic redux-saga Setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, createStore, applyMiddleware} from "redux";
+import createSagaMiddleware from 'redux-saga'
 import counter from './counter'
+import user from './user'
 const reducers = combineReducers({
-    counter: counter
+    counter: counter,
+    user: user
 })
 
-const store = createStore(reducers)
+const sagaMiddleware = createSagaMiddleware()
+
+const middleware = [sagaMiddleware]
+
+const store = createStore(reducers, {}, applyMiddleware(...middleware))
 
 export default store
---
+++ src/store/user/index.js
@@ -0,0 +1,28 @@
+export const GET_USER = "GET_USER"
+export const SET_USER = "SET_USER"
+
+export const getUser = () => ({
+    type: GET_USER
+})
+
+export const setUser = (user) => ({
+    type: SET_USER,
+    user
+})
+
+const initialState = {
+    count: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch  (action.type){
+        case SET_USER:
+            const {user} = action
+            return {
+            ...state, user
+        }
+        default: return state
+    }
+}
+
+export default reducer
